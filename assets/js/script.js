77de876400b154a38b7f785dac36760b452e074f
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-07-04 20:46:36 -0600

    This updated it has new features and images with the different effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,3 +77,83 @@ window.addEventListener("scroll", function () {
     backTopBtn.classList.remove("active");
   }
 });
+
+// hero slider action 
+
+const heroSlider = document.querySelector("[data-hero-slider]");
+const heroSliderItems = document.querySelectorAll("[data-hero-slider-item]");
+const heroSliderPrevBtn = document.querySelector("[data-prev-btn]");
+const heroSliderNextBtn = document.querySelector("[data-next-btn]");
+
+let currentSlidePos = 0;
+let lastActiveSliderItem = heroSliderItems[0];
+
+const updateSliderPos = function () {
+  lastActiveSliderItem.classList.remove("active");
+  heroSliderItems[currentSlidePos].classList.add("active");
+  lastActiveSliderItem = heroSliderItems[currentSlidePos];
+}
+
+const slideNext = function () {
+  if (currentSlidePos >= heroSliderItems.length - 1) {
+    currentSlidePos = 0;
+  } else {
+    currentSlidePos++;
+  }
+
+  updateSliderPos();
+}
+
+heroSliderNextBtn.addEventListener("click", slideNext);
+
+const slidePrev = function () {
+  if (currentSlidePos <= 0) {
+    currentSlidePos = heroSliderItems.length - 1;
+  } else {
+    currentSlidePos--;
+  }
+
+  updateSliderPos();
+}
+
+heroSliderPrevBtn.addEventListener("click", slidePrev);
+
+/**
+ * auto slide
+ */
+
+let autoSlideInterval;
+
+const autoSlide = function () {
+  autoSlideInterval = setInterval(function () {
+    slideNext();
+  }, 7000);
+}
+
+addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseover", function () {
+  clearInterval(autoSlideInterval);
+});
+
+addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseout", autoSlide);
+
+window.addEventListener("load", autoSlide);
+
+// Parallax effect
+const parallaxItems = document.querySelectorAll("[data-parallax-item]");
+let x, y;
+window.addEventListener("mousemove", function (event) {
+  x=(event.clientX / window.innerWidth * 10)-5;
+  y=(event.clientY / window.innerHeight * 10)-5;
+for (let i = 0, len= parallaxItems.length; i < len; i++) {
+x = x * Number(parallaxItems[i].dataset.parallaxSpeed);
+y = y * Number(parallaxItems[i].dataset.parallaxSpeed);
+parallaxItems[i].style.transform = `translate3d(${x}px, ${y}px, 0px)`;
+}
+});
+
+
+
+
+
+
+
